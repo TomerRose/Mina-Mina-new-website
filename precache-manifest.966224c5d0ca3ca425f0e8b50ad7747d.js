f25bdc6304b98c36e38835c5dc0298c2fc4ac7d4
TomerRO <tomerrose141@gmail.com>
2020-08-31 13:56:39 +0300

    Updates

---
+++ precache-manifest.966224c5d0ca3ca425f0e8b50ad7747d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d16a7a92658eae218648c944830328",
+    "url": "/Mina-Mina-new-website/index.html"
+  },
+  {
+    "revision": "832f9f00bfb344438140",
+    "url": "/Mina-Mina-new-website/static/css/main.61e7e750.chunk.css"
+  },
+  {
+    "revision": "57e12cadace84a83f7f7",
+    "url": "/Mina-Mina-new-website/static/js/2.71b0eaa7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mina-Mina-new-website/static/js/2.71b0eaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832f9f00bfb344438140",
+    "url": "/Mina-Mina-new-website/static/js/main.05bb1483.chunk.js"
+  },
+  {
+    "revision": "beb80f7d845a8f6886e6",
+    "url": "/Mina-Mina-new-website/static/js/runtime-main.a96e0cf6.js"
+  }
+]);
